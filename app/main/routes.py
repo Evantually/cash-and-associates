d86d1db6f4f00dca01b832fad72a301024709275
Evantually <egbeesefl@gmail.com>
2021-08-20 20:01:52 -0500

    # of kills, etc.

--- app/main/routes.py
+++ app/main/routes.py
@@ -401,7 +401,8 @@ def hunting_tracker(job_id):
 @login_required
 def hunting_jobs():
     jobs = Job.query.filter_by(user_id=current_user.id).order_by(Job.timestamp.desc()).all()
-    return render_template('jobs_overview.html', jobs=jobs)
+    ma_data, time_data = moving_average(entries, 2, 30)
+    return render_template('jobs_overview.html', jobs=jobs, values=ma_data, labels=time_data)
 
 @bp.route('/jobs/hunting/view/<job_id>')
 @login_required
